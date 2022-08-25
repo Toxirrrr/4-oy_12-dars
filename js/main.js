fdeb5c77641bb8c96a3f00c3d0b9bc0538e03593
Toxirrrr <mjuraeva74@gmail.com>
2022-08-26 02:21:39 +0500

    push

--- js/main.js
+++ js/main.js
@@ -7,25 +7,18 @@ let elTempIsAdmin = document.querySelector(".user__isAdmin");
 let elTempEdit = document.querySelector(".user__edit");
 let elTempRemove = document.querySelector(".user__remove");
 let elTempId = document.querySelector(".user__id");
-let elMe = document.querySelector(".me");  
 let result = document.querySelector(".result");  
 let elBtn = document.querySelector(".wrapper__btn")
 let elBtnDelete = document.querySelector(".wrapper__btn-delete")
 let elBtnEdit = document.querySelector(".wrapper__btn-edit")
 let elBtnSsave = document.querySelector(".wrapper__btn-save")
+let authToken = localStorage.getItem("token");
+let elMe = document.querySelector(".me");  
 let me = elMe;
 
-let authToken = localStorage.getItem("token");
 
-elBtnLogOut.addEventListener("click", function() {
-    
-    localStorage.removeItem("token")
-    
-    window.location.href = "/login.html"
-    
-})
 
-function renderUsers(array) {
+function renderPosts(array) {
     
     elWrapper.innderHTML = null;
     
@@ -35,43 +28,17 @@ function renderUsers(array) {
         
         let rowTemp = elRowTemp.cloneNode(true);
         
-        rowTemp.querySelector(".user__name").textContent = item.name;
-        rowTemp.querySelector(".user__email").textContent = item.email;
-        // rowTemp.querySelector(".user__id").textContent = item._id;
+        rowTemp.querySelector(".user__title").textContent = item.title;
+        rowTemp.querySelector(".user__body").textContent = item.body;
+        rowTemp.querySelector(".user__id").textContent = item._id;
         
         newFragment.appendChild(rowTemp)
     }
     elWrapper.appendChild(newFragment)
 }
 
-// function renderMe(array) {
 
-//     elTable.innderHTML = null;
-
-//     let newFragment1 = document.createDocumentFragment()
-
-//     for (const item of array) {
-
-//         let headerTemp = elHeaderTemp.cloneNode(true);
-
-//         headerTemp.querySelector(".header__user-name").textContent = item.name;
-
-//         headerTemp.querySelector(".header__user-email").textContent = item.email;
-
-//         newFragment1.appendChild(headerTemp)
-//     }
-
-//     elTable.appendChild(newFragment1)
-
-// } 
-
-// let elBtnLogOut = document.querySelector(".log_out");
-
-// let elRowTemplate = document.querySelector("#rowTemp").content;
-
-// let elWrapper = document.querySelector(".wrapper");
-
-fetch("https://fast-ravine-16741.herokuapp.com/api/users", {
+fetch("https://fast-ravine-16741.herokuapp.com/api/posts", {
 method: 'GET',
 headers: {
     "Content-Type":"application/json",
@@ -81,11 +48,19 @@ headers: {
 .then(data => 
     {
         if (!data.error) {
-            renderUsers(data)
-            result.innerHTML = data.length;
+            renderPosts(data.posts)
+            result.innerHTML = data.totalResults
         }else {
-            window.location.href = "/login.html"
+            window.location.href = "/login.html"    
         }
+    })
+    
+    elBtnLogOut.addEventListener("click", function() {
+        
+        localStorage.removeItem("token")
+        
+        window.location.href = "/login.html"
+        
     })
     
     
@@ -99,69 +74,7 @@ headers: {
     .then(data => 
         {
             if (!data.error) {
-                elMe.innerHTML=data.email
-            }else {
-                window.location.href = "/login.html"    
+                elMe.innerHTML=data.name
             }
         })
-        
-        
-        
-        
-        // fetch("https://fast-ravine-16741.herokuapp.com/api/users/me", {
-        // method: 'GET',
-        // headers: {
-        //     "Content-Type":"application/json",
-        //     "Authorization": authToken
-        // }})
-        // .then(res => res.json())
-        // .then(data => 
-        //     {
-        //         if (!data.error) {
-        //             renderMe([data]);
-        //         }
-        //     })
-        
-        
-        
-        //    {
-        //         if (!data.error) {
-        //             renderUsers(data);
-        //         }else {
-        //             window.location.href = "/login.html"
-        //         }
-        //     })
-        
-        //     elBtnLogOut.addEventListener("click", function() {
-        //         localStorage.removeItem("token")
-        
-        //         window.location.href = "/login.html"
-        //     })
-        
-        
-        //     function renderUsers(array) {
-        //         elWrapper.innerHTML = null;
-        
-        //         let newFragment = document.createDocumentFragment()
-        
-        //         for (const item of array) {
-        //            let rowTemp = elRowTemplate.cloneNode(true);
-        
-        //            rowTemp.querySelector(".user__name").textContent = item.name;
-        //            rowTemp.querySelector(".user__email").textContent = item.email;
-        //            rowTemp.querySelector(".user__isAdmin").textContent = item.isAdmin;
-        //            rowTemp.querySelector(".user__id").textContent = item._id;
-        
-        //            newFragment.appendChild(rowTemp);
-        //         }
-        //         elWrapper.appendChild(newFragment);
-        //     }
-        
-        
-        
-        
-        
-        
-        
-        
         
